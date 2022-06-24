e2aaebc25275b7ff1e946d452c7d47bca2b36cd3
unknown <waynexu111@gmail.com>
2022-06-25 06:37:57 +1000

    added some stubs for movie api

--- src/main/java/com/example/restservice/api/UserController.java
+++ src/main/java/com/example/restservice/api/UserController.java
@@ -29,22 +29,23 @@ public class UserController {
         this.userService = userService;
     }
 
-    // @PostMapping("/login")
-    // public Object authenticateUser(@RequestBody User user) {
-    //     JSONObject response = userService.authenticateUser(user);
+    @PostMapping("/login")
+    public Object authenticateUser(@RequestBody User user) {
+        JSONObject response = userService.authenticateUser(user);
 
-    //     if(response.has("error")){
-    //         return new ResponseEntity<Object>(response.toString(), HttpStatus.BAD_REQUEST);
-    //     } else {
-    //         return new ResponseEntity<Object>(response.toString(), HttpStatus.OK);
-    //     }
-    // }
+        if(response.has("error")){
+            return new ResponseEntity<Object>(response.toString(), HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity<Object>(response.toString(), HttpStatus.OK);
+        }
+    }
 
 
     @PostMapping("/register")
     public ResponseEntity<Object> addUser(@RequestBody User user) {
 
-        JSONObject response = userService.addUser(user);
+        // admin accounts are created in backend, if calling this api, assume it is from frontend (therefore admin = false)
+        JSONObject response = userService.addUser(user, false);
 
         if(response.has("error")){
             return new ResponseEntity<Object>(response.toString(), HttpStatus.BAD_REQUEST);
@@ -58,6 +59,41 @@ public class UserController {
     @GetMapping("/getAllUsers")
     public List<User> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+
+    @GetMapping("/movie")
+    public Object getMovieDetails(long id) {
+        JSONObject response = userService.getMovieDetails(id);
+
+        // TODO: test the error codes work
+        if(response.has("error")){
+            String error = response.getString("error");
+            if (error.equals("Invalid input")) {
+                return new ResponseEntity<Object>(response.toString(), HttpStatus.BAD_REQUEST);
+            } else {
+                return new ResponseEntity<Object>(response.toString(), HttpStatus.NOT_FOUND);
+            }
+        } else {
+            return new ResponseEntity<Object>(response.toString(), HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/movie/search")
+    public Object searchMovieByName(String name) {
+        JSONObject response = userService.searchMovieByName(name);
+
+        // TODO: test the error codes work
+        if(response.has("error")){
+            String error = response.getString("error");
+            if (error.equals("Invalid input")) {
+                return new ResponseEntity<Object>(response.toString(), HttpStatus.BAD_REQUEST);
+            } else {
+                return new ResponseEntity<Object>(response.toString(), HttpStatus.NOT_FOUND);
+            }
+        } else {
+            return new ResponseEntity<Object>(response.toString(), HttpStatus.OK);
+        }
     }
 
 
--- src/main/java/com/example/restservice/service/UserService.java
+++ src/main/java/com/example/restservice/service/UserService.java
@@ -1,15 +1,15 @@
 package com.example.restservice.service;
 
 import java.util.List;
-import java.util.UUID;
 
 import com.example.restservice.dataModels.User;
 import com.example.restservice.database.UserDataAccessService;
 
+import com.example.restservice.dataModels.Movie;
+
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.json.JSONObject;
 
 import java.util.HashMap;
@@ -22,75 +22,115 @@ public class UserService {
     @Autowired
 	private UserDataAccessService userDAO;
 
+    // receives email and password, check db for valid user
+    public JSONObject authenticateUser(User user) {
 
-    // public JSONObject authenticateUser(User user) {
-
-    //     // // TODO: check user values for errors
-    //     // if () {
-    //     //     return invalidInputError();
-    //     // }
-
-
-    //     HashMap<String,Object> returnMessage = new HashMap<String,Object>();
-
-    //     // find the user in the database
-    //     User dbUser = userDAO.getByEmail(user.getEmail());
-    //     if (dbUser != null) {
-    //         // TODO: Add token implementation
-    //         returnMessage.put("userId", dbUser.getId());
-    //         returnMessage.put("isAdmin", dbUser.getIsAdmin());
-    //     }
-    //     // otherwise return error
-    //     else {
-    //         return invalidInputError();
-    //     }
-
-    //     JSONObject responseJson = new JSONObject(returnMessage);
-    //     return responseJson;
-
-
-    // }
-
-    // public JSONObject addUser(User user) {
-    //     // generate random ID for now
-    //     UUID id = UUID.randomUUID();
-
-    //     // // TODO: check user values for errors
-    //     // if () {
-    //     //     return invalidInputError();
-    //     // }
-
-
-    //     HashMap<String,Object> returnMessage = new HashMap<String,Object>();
-
-    //     // if user is added, return details
-    //     if (userDAO.addUser(user, id) == 1) {
-    //         // TODO: Add token implementation
-    //         returnMessage.put("userId", id);
-    //     }
-    //     // otherwise return error
-    //     else {
-    //         return invalidInputError();
-    //     }
-
-    //     JSONObject responseJson = new JSONObject(returnMessage);
-    //     return responseJson;
-    // }
-
-    public JSONObject addUser(User user) {
-        HashMap<String,Object> returnMessage = new HashMap<String,Object>();
-        returnMessage.put("userId", "test");
-        if (userDAO.save(user) != null) {
-            JSONObject responseJson = new JSONObject(returnMessage);
-            return responseJson;
+        // TODO: check user values for errors
+        if (!checkEmail(user.getEmail()) || !checkPassword(user.getPassword())) {
+            return invalidInputError();
         }
-        return invalidInputError();
 
+        HashMap<String,Object> returnMessage = new HashMap<String,Object>();
+
+        // TODO: Query the database by email to find a matching user, put user in "dbUser"
+        User dbUser = user;
+        // TODO: if user found
+        if (dbUser != null) {
+            // TODO: Add token implementation
+            returnMessage.put("userId", dbUser.getId());
+            returnMessage.put("isAdmin", dbUser.getIsAdmin());
+        }
+        // otherwise return error
+        else {
+            return invalidInputError();
+        }
+
+        JSONObject responseJson = new JSONObject(returnMessage);
+        return responseJson;
+
+    }
+
+    // receives name, email, password
+    public JSONObject addUser(User user, Boolean isAdmin) {
+
+        user.setIsAdmin(isAdmin);
+
+        // TODO: check user values for errors
+        if (!checkName(user.getName()) || !checkEmail(user.getEmail()) || !checkPassword(user.getPassword())) {
+            return invalidInputError();
+        }
+
+        HashMap<String,Object> returnMessage = new HashMap<String,Object>();
+
+        try{
+            // if user is successfully added, put user in dbUser
+            User dbUser = userDAO.save(user);
+
+            // set return response values
+            // TODO: Add token implementation
+            returnMessage.put("userId", dbUser.getId());
+        } catch(IllegalArgumentException e){
+            return invalidInputError();
+        }
+
+        JSONObject responseJson = new JSONObject(returnMessage);
+        return responseJson;
     }
 
 
     public List<User> getAllUsers() {
         return userDAO.findAll();
+    }
+
+    public JSONObject getMovieDetails(long id) {
+
+        HashMap<String,Object> returnMessage = new HashMap<String,Object>();
+
+        
+        // TODO: Query the database by movie id, need to get actors(cast), genres and reviews as well
+        Movie dbMovie = new Movie();
+        // TODO: if movie found
+        if (dbMovie != null) {
+            returnMessage.put("name", dbMovie.getName());
+            returnMessage.put("year", dbMovie.getYear());
+            returnMessage.put("poster", dbMovie.getPoster());
+            returnMessage.put("description", dbMovie.getDescription());
+            returnMessage.put("director", dbMovie.getDirector());
+            //TODO: add genres
+            returnMessage.put("contentRating", dbMovie.getContentRating());
+            returnMessage.put("averageRating", dbMovie.getAverageRating());
+            //TODO: add cast
+            //TODO: add reviews
+        }
+
+        JSONObject responseJson = new JSONObject(returnMessage);
+        return responseJson;
+
+    }
+
+
+    public JSONObject searchMovieByName(String name) {
+
+        HashMap<String,Object> returnMessage = new HashMap<String,Object>();
+
+        // TODO: fix return format (list of objects)
+
+        // TODO: Query the database by movie name, need to get genres as well
+        Movie dbMovie = new Movie();
+        // TODO: if movie found
+        if (dbMovie != null) {
+            returnMessage.put("id", dbMovie.getId());
+            returnMessage.put("name", dbMovie.getName());
+            returnMessage.put("year", dbMovie.getYear());
+            returnMessage.put("poster", dbMovie.getPoster());
+            returnMessage.put("description", dbMovie.getDescription());
+            //TODO: add genres
+            returnMessage.put("averageRating", dbMovie.getAverageRating());
+        }
+
+        JSONObject responseJson = new JSONObject(returnMessage);
+        return responseJson;
+
     }
 
     private JSONObject invalidInputError() {
@@ -105,5 +145,19 @@ public class UserService {
         returnMessage.put("error", "Not found");
         JSONObject responseJson = new JSONObject(returnMessage);
         return responseJson;
+    }
+
+
+    // TODO: checks if name is valid format, true = valid
+    private Boolean checkName(String name) {
+        return true;
+    }
+    // TODO: checks if email is valid format, true = valid
+    private Boolean checkEmail(String email) {
+        return true;
+    }
+    // TODO: checks if password is valid format, true = valid
+    private Boolean checkPassword(String Password) {
+        return true;
     }
 }
