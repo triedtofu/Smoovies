54d0ac664ebd33631044f7cc5e76974fa7b12454
Raymond Wang <raymond.wang2@student.unsw.edu.au>
2022-06-25 23:49:24 +1000

    added wishlist as a  hyperlink for easy access to page (remove once api calls are ready)

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -9,13 +9,13 @@ const Navbar = () => {
       <div>
         <Link to="/">Home</Link>
       </div>
-
       <div className={styles.nav_right}>
+        <Link to="/token/wishlist">Wishlist</Link>
         <Link to="/login">Login</Link>
         <Link to="/register">Register</Link>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
