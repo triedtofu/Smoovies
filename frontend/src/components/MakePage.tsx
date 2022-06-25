763da60487bf7f432435db0973b6607ec5690d56
Raymond Wang <raymond.wang2@student.unsw.edu.au>
2022-06-25 23:48:25 +1000

    minor styling changes

--- frontend/src/components/MakePage.tsx
+++ frontend/src/components/MakePage.tsx
@@ -3,13 +3,12 @@ import React from 'react';
 import Navbar from './Navbar';
 
 const MakePage = (Component: React.ElementType) => {
-
   return () => (
     <>
       <Navbar />
       <Component />
     </>
   );
-}
+};
 
 export default MakePage;
