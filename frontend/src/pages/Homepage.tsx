1817629b9048aa86ae43633d74c4c2b87a0476cd
Raymond Wang <raymond.wang2@student.unsw.edu.au>
2022-06-26 01:53:12 +1000

    token authentication for invalid cases

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 import MakePage from '../components/MakePage';
+import { useNavigate } from 'react-router-dom';
 
-const Homepage = () => {
-  return (
-    <h1>Home Page</h1>
-  );
+function Homepage() {
+  const navigate = useNavigate();
+
+  // code for when I can get token
+  // React.useEffect(() => {
+  //   if (!token) {
+  //     navigate('/login');
+  //   }
+  // });
+
+  return <h1>Home Page</h1>;
 }
 
 export default MakePage(Homepage);
