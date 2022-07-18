17fdae3b82ea58ae417a9f2e3d03f022be92e04d
Leighton Fan <leighton.l.fan@gmail.com>
2022-07-18 17:33:23 +1000

    Added basic add review functionality

--- frontend/src/components/ReviewCard.tsx
+++ frontend/src/components/ReviewCard.tsx
@@ -1,43 +1,28 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
 
 import Rating from '@mui/material/Rating';
 import StarIcon from '@mui/icons-material/Star';
+
+import styles from './ReviewCard.module.css';
+
+import MyLink from './MyLink';
 
 import { Review } from '../util/interface';
 
 
 const ReviewCard = ({ review }: { review: Review }) => {
   return (
-    <div>
-      <div
-        style={{
-          paddingBottom: '10px',
-          paddingLeft: '30px',
-          paddingRight: '30px',
-          border: '1px solid black',
-          borderRadius: '5px',
-        }}
-      >
-        <div
-          style={{
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            display: 'flex',
-          }}
-        >
-          {review.name}
-          &nbsp;&nbsp;&nbsp;
-          <Rating
-            name="text-feedback"
-            value={review.rating}
-            readOnly
-            precision={0.1}
-            emptyIcon={<StarIcon style={{ opacity: 0.7 }} fontSize="inherit" />}
-          />
-        </div>
-        <br />"{review.review}"
+    <div className={styles.reviewOuter}>
+      <div className={styles.reviewHeader}>
+        <MyLink to={`/user/${review.user}`}>{review.name}</MyLink>
+        <Rating
+          name="text-feedback"
+          value={review.rating}
+          readOnly
+          emptyIcon={<StarIcon style={{ opacity: 0.7 }} fontSize="inherit" />}
+        />
       </div>
+      <div>"{review.review}"</div>
     </div>
   );
 };
--- frontend/src/pages/Movie.tsx
+++ frontend/src/pages/Movie.tsx
@@ -5,14 +5,19 @@ import { useCookies } from 'react-cookie';
 import styles from './Movie.module.css';
 import MakePage from '../components/MakePage';
 import Youtube from '../components/Youtube';
+import ReviewCard from '../components/ReviewCard';
+import ReviewInput from '../components/ReviewInput';
 
 import Button from '@mui/material/Button';
-import Rating from '@mui/material/Rating';
-import TextareaAutosize from '@mui/material/TextareaAutosize';
-import ReviewCard from '../components/ReviewCard';
 import Container from '@mui/material/Container';
 
-import { apiGetMovie, apiUserWishlist, apiPutUserWishlist, apiDeleteMovie } from '../util/api';
+import {
+  apiGetMovie,
+  apiUserWishlist,
+  apiPutUserWishlist,
+  apiDeleteMovie,
+  apiAddReview
+} from '../util/api';
 import { parseJwt, getErrorMessage } from '../util/helper';
 import { SpecificMovieResponse } from '../util/interface';
 
@@ -161,15 +166,15 @@ const Movie = () => {
     }
   };
 
-  const submitReview = () => {
-    // TODO
+  const submitReview = (rating: number, review: string) => {
+    apiAddReview(cookies.token, parseInt(params.id!), review, rating);
   };
 
   if (!movie) return <></>;
 
   return (
     <Container maxWidth="md">
-      <div className={styles.title_div}>
+      <div className={styles.titleDiv}>
         <h1>
           {movie.name} ({movie.year})
         </h1>
@@ -213,7 +218,7 @@ const Movie = () => {
 
       <div>
         <h2>Reviews</h2>
-        <div style={{ display: 'flex' }}>
+        <div className={styles.reviewsDiv}>
           {movie.reviews.map(review => (
             <ReviewCard
               key={review.user}
@@ -222,43 +227,10 @@ const Movie = () => {
           ))}
         </div>
       </div>
-      <br />
-      <div>
-        <div
-          style={{
-            paddingBottom: '10px',
-            paddingLeft: '30px',
-            border: '1px solid black',
-          }}
-        >
-          <div
-            style={{
-              alignItems: 'center',
-              justifyContent: 'space-between',
-              display: 'flex',
-            }}
-          >
-            <h2>Write a Review</h2>
-            <Rating
-              style={{ paddingRight: '30px' }}
-              name="half-rating"
-              defaultValue={2.5}
-              precision={0.5}
-            />
-          </div>
-          <TextareaAutosize
-            aria-label="minimum height"
-            minRows={5}
-            placeholder="Write your review here"
-            style={{ width: '95%' }}
-          />
-          <br />
-          <Button size="small" variant="contained" onClick={submitReview}>
-            Submit
-          </Button>
-          <br />
-        </div>
-      </div>
+      <br/>
+
+      {cookies.token ? <ReviewInput submitReview={submitReview} /> : <p>Login/Register to write a review!</p>}
+         
     </Container>
   );
 };
