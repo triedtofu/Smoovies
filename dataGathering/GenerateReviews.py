b785ddc9dc4a6753174abb68ef95d5240194cc2d
Kane Qian <z5259933@ad.unsw.edu.au>
2022-07-29 18:29:01 +1000

    added 20 review templates for GenerateReviews.py

--- dataGathering/GenerateReviews.py
+++ dataGathering/GenerateReviews.py
@@ -6,21 +6,45 @@ registerUrl = 'http://localhost:8080/api/user/register'
 loginUrl = 'http://localhost:8080/api/user/login'
 addReviewUrl = 'http://localhost:8080/api/movie/addReview'
 
-reviewer = {"name": "Reviewer One", "email": "reviewer@email.com", "password": "Password123"}
-reviewerInfo = requests.post(registerUrl, json = reviewer)  
+reviewerRegister = {"name": "Reviewer Three", "email": "reviewer3@email.com", "password": "Password123"}
+#reviewerLogin = {"email": "reviewer2@email.com", "password": "Password123"}
+reviewerInfo = requests.post(registerUrl, json = reviewerRegister)  
+#reviewerInfo = requests.post(loginUrl, json = reviewerLogin)
 print(reviewerInfo.json())
-review1 = "It's Morbin Time"
 
-for x in range(1, 450):
+review1 = "It's Morbin Time"
+review2 = "It's Not Morbin Time"
+review3 = "This is a dog movie don't watch it"
+review4 = "I'm just leaving a review for the sake of leaving a review"
+review5 = "I love writing reviews"
+review6 = "Only smelly people like this movie"
+review7 = "Only sexy people like this movie"
+review8 = "Give me a refund on my movie ticket please"
+review9 = "Best movie of all time"
+review10 = "Worst movie of all time"
+review11 = "I'm running out of ideas"
+review12 = "I'm in love with the main character"
+review13 = "I reckon I could've done a better fake accent"
+review14 = "Director deserves an award"
+review15 = "Looks like it was made for a HSC drama project"
+review16 = "I just realised that these preset reviews may not correlate with the randomly generated ratings"
+review17 = "The reviewer below is stupid"
+review18 = "Review18"
+review19 = "Almost there (Review19)"
+review20 = "My work here is done"
+
+reviewList = [review1, review2, review3, review4, review5, review6, review7, review8, review9, review10, 
+              review11, review12, review13, review14, review15, review16, review17, review18, review19, review20]
+for x in range(1, 350):
     review = {"token" : [], "movieId" : [], "review" : [], "rating" : []}
     review["token"] = (reviewerInfo.json())["token"]
     review["movieId"] = x
-    review["review"] = review1
-    review["rating"] = random.randint(0,5)
-    x = requests.post(addReviewUrl, json = review)
+    review["review"] = reviewList[random.randint(0,19)]
+    review["rating"] = random.randint(1,5)
+    y = requests.post(addReviewUrl, json = review)
 
-    if x.status_code == 200:
-        print("Review added successfully")
+    if y.status_code == 200:
+        print("Review", x, "added successfully")
 
     else:
         print("Could not add review")
